da78c988b378c848161146b81f134a8e528bf20a
davidocta123 <davi20269ti@student.nurulfikri.ac.id>
2023-06-18 12:05:34 -0700

    revisi fe-Holyways

--- src/components/DetailDonate.jsx
+++ src/components/DetailDonate.jsx
@@ -44,25 +44,8 @@ function DetailDonate() {
   });
 
   
+
   const handleDonate = useMutation(async (e) => {
-    useEffect(() => {
-      //change this to the script source you want to load, for example this is snap.js sandbox env
-      const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
-      //change this according to your client-key
-      const myMidtransClientKey = "SB-Mid-client-jwwep56vrgNs5z23";
-  
-      let scriptTag = document.createElement("script");
-      scriptTag.src = midtransScriptUrl;
-      // optional if you want to set script attribute
-      // for example snap.js have data-client-key attribute
-      scriptTag.setAttribute("data-client-key", myMidtransClientKey);
-  
-      document.body.appendChild(scriptTag);
-      return () => {
-        document.body.removeChild(scriptTag);
-      };
-    }, []);
-    
     try {
       e.preventDefault();
 
@@ -110,6 +93,24 @@ function DetailDonate() {
       console.log("donation failed : ", error);
     }
   });
+
+  useEffect(() => {
+    //change this to the script source you want to load, for example this is snap.js sandbox env
+    const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
+    //change this according to your client-key
+    const myMidtransClientKey = "SB-Mid-client-jwwep56vrgNs5z23";
+
+    let scriptTag = document.createElement("script");
+    scriptTag.src = midtransScriptUrl;
+    // optional if you want to set script attribute
+    // for example snap.js have data-client-key attribute
+    scriptTag.setAttribute("data-client-key", myMidtransClientKey);
+
+    document.body.appendChild(scriptTag);
+    return () => {
+      document.body.removeChild(scriptTag);
+    };
+  }, []);
 
   return (
     <div className="" style={{ backgroundColor: "#E5E5E5" }}>
